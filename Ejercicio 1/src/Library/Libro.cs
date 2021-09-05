0380d69553c5f0b55dd33e652426da975b4f1090
lukaszury <lukaszury@gmail.com>
2021-09-05 18:23:42 -0300

    Ejercicio1-justificacion

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -4,10 +4,13 @@ namespace SRP
 {
     public class Libro
     {
-
-        public string Titulo { get ; }
+public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
+        /*
+        Los atributos SectorBiblioteca y EstanteBiblioteca podrian ser properties de una clase que contenga los libros ya que si a estos les damos
+        la responsabilidad de almacenarse a si mismos no estariamos cumpliendo con la SRP
+        */
         public string SectorBiblioteca { get ; set; }
         public string EstanteBiblioteca { get ; set; }
 
@@ -18,6 +21,10 @@ namespace SRP
             this.Codigo = codigo;
         }
 
+        /* 
+        Los libros no deberian ser los responsables de almacenarse, lo mejor seria tener una clase llamada Biblioteca, la cual sera
+        responsable de guardar los libros
+        */
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,8 +9,12 @@ namespace SRP
         {
             Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034");
             Libro libro2 = new Libro("Pro C#","Troelsen","001-035");
+            /*
+            Estos metodos deberian ser llamados de otra clase, una que sea responsable de manipular los libros, en este caso, almacenarlos
+            */
             libro1.AlmacenarLibro("A","7");
             libro2.AlmacenarLibro("B","3");
+
 
         }
     }
